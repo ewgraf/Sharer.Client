ed8e1ca2aee38ce8f64ee5da18ad2324deaf750a
Ewgraf <ewgraf@mail.ru>
2019-03-07 11:44:58 +0300

    msi building project

---
+++ Sharer.Client.MSI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using WixSharp;
+
+namespace Sharer.Client.MSI {
+	public class Script {
+		public static void Main(string[] args) {
+			string prefix = @"..\Sharer.Client\bin\Release";
+			var project = new Project("Sharer",
+				new Dir(@"%ProgramFiles%\Sharer",
+					new File($@"{prefix}\sharer.exe", 
+						new FileShortcut("Sharer", @"%AppData%\Microsoft\Windows\Start Menu\Programs")
+					),
+					new File($@"{prefix}\sharer.exe.config"),
+					new File($@"{prefix}\Microsoft.WindowsAPICodePack.dll"),
+					new File($@"{prefix}\Microsoft.WindowsAPICodePack.Shell.dll")
+				)
+			);
+
+			project.GUID = new Guid("56628ce2-91ad-464c-a005-a19e09a5c9a2");
+
+			Compiler.BuildMsi(project);
+		}
+	}
+}
